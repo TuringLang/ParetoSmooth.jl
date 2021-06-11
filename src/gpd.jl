4500f0256b824ae0c99cd14a68e53a009a649460
Closed-Limelike-Curves <paradac@carleton.edu>
2021-06-10 19:51:49 -0700

    Original quartile estimator

--- src/gpd.jl
+++ src/gpd.jl
@@ -1,71 +1,72 @@
 module gpd
 export gpdfit, gpd_quantile
 
-using Memoization, Statistics, LinearAlgebra
+using Memoization, Statistics, LinearAlgebra, Tullio, LoopVectorization, KernelAbstractions
 
 
 """
-    gpdfit(x::AbstractArray, wip::Bool = true, min_grid_pts::Int = 30, wip = true, sort_x::Bool = true)
+    gpdfit(sample::AbstractArray, wip::Bool = true, min_grid_pts::Int = 30, wip = true, sort_sample::Bool = true)
 
-Given a sample \$x\$, estimate the parameters \$k\$ and \$\\sigma\$ of the generalized Pareto 
-distribution (GPD), assuming the location parameter is 0. The fit uses
-a weak prior for \$k\$, which will stabilize estimates for very small sample 
-sizes (and low effective sample sizes in the case of MCMC samples). 
-The weakly informative prior is a Gaussian centered at 0.5. 
+Given a sample, estimate the parameters \$ξ\$ and \$\\sigma\$ of the  
+generalized Pareto distribution (GPD), assuming the location parameter
+is 0. The fit uses a weak prior for \$ξ\$, which will stabilize estimates 
+for very small sample sizes (and low effective sample sizes in the case of 
+MCMC samples). The weakly informative prior is a Gaussian centered at 0.5. 
 
 
 # Arguments
-- `x::AbstractArray`: A numeric vector. The sample from which to estimate the parameters.
-- `wip::Bool = true`: Logical indicating whether to adjust k based on a weakly
+- `sample::AbstractArray`: A numeric vector. The sample from which to estimate the parameters.
+- `wip::Bool = true`: Logical indicating whether to adjust \$ξ\$ based on a weakly
   informative Gaussian prior centered on 0.5. Defaults to true.
 - `min_grid_pts::Int = 30`: The minimum number of grid points used in the fitting
-  algorithm. The actual number used is `min_grid_pts + floor(sqrt(length(x)))`.
-- `sort_x::Bool = true`: If `true`, the first step in the fitting
-  algorithm is to sort the elements of `x`. If `x` is already
-  sorted in ascending order then `sort_x` can be set to `false` to
+  algorithm. The actual number used is `min_grid_pts + floor(sqrt(length(sample)))`.
+- `sort_sample::Bool = true`: If `true`, the first step in the fitting
+  algorithm is to sort the elements of `sample`. If `sample` is already
+  sorted in ascending order then `sort_sample` can be set to `false` to
   skip the initial sorting step.
-- A named list with components `k` and `sigma`.
 
 
 # Returns
-- `k, σ`: The estimated parameters of the generalized Pareto distribution.
+- `ξ, σ`: The estimated parameters of the generalized Pareto distribution.
 
 # Note
-The parameter \$k\$ is the negative of \$k\$ in [zhangNewEfficientEstimation2009](@cite).
-A slightly different quantile interpolation is used than in the paper.
+The parameter \$ξ\$ is the negative of \$k\$ in [zhangNewEfficientEstimation2009](@cite).
+A slightly different quantile interpolation is used than in the paper. This function will
+modify the provided sample by sorting it to speed up future applications of gpdfit.
 """
-function gpdfit(x::AbstractVector, wip::Bool = true, min_grid_pts::Int = 30, sort_x::Bool = true)
+function gpdfit(sample::AbstractVector, wip::Bool = true, min_grid_pts::Int = 30, sort_sample::Bool = true)
   
-    
-    # x must be sorted, but we can skip if x is already sorted
-    if sort_x
-        sort!(x)
-    end
+  n = length(sample)
 
-    n = length(x)
-    prior = 3.0
-    m = min_grid_pts + isqrt(n) # isqrt = floor sqrt
-    n_0 = 10.0  # determines how strongly to nudge ξ towards .5
-    quartile = quantile(x, .25; sorted = true, alpha = 0)
-    seq = collect(1:m)
-    
+  # sample must be sorted, but we can skip if sample is already sorted
+  if sort_sample
+      sort!(sample)
+  end
 
-    # build pointwise estimates of k and θ by using each element of the sample.
-    θHats = @. 1 / x[n] .+ (1 - sqrt((m+1) / seq)) / prior ./ quartile
-    ξHats = mean(log1p.(- θHats .* x'), dims = 2)  # take mean f each row
-    logLikelihood = @. n * (log(- θHats / ξHats) - ξHats - 1)  # Calculate log-likelihood at each estimate
-    weights = 1 ./ sum(exp.(logLikelihood .- logLikelihood'), dims = 1)' # Calculate weights from log-likelihood
+  
+  prior = 3.0
+  m = min_grid_pts + isqrt(n) # isqrt = floor sqrt
+  n_0 = 10.0  # determines how strongly to nudge ξ towards .5
+  quartile = sample[(n+2) ÷ 4] 
+  seq = collect(1:m) 
 
-    θHat = weights' ⋅ θHats  # Take the dot product of weights and pointwise estimates of θ to get the full estimate
 
-    ξ = mean(log1p.(- θHat .* x))
-    σ = -ξ / θHat
-    # Drag towards .5 to reduce variance for small n
-    if wip
-      ξ = (ξ * n + .5 * n_0) / (n + n_0) 
-    end
+  # build pointwise estimates of k and θ by using each element of the sample.
+  @turbo θHats = @. 1 / sample[n] + (1 - sqrt(m / (seq - .5))) / prior / quartile
+  @tullio grad=false ξHats[x] := log1p(- θHats[x] * sample[y]) |> (_ / n)
+  @turbo logLikelihood = @. n * (log(- θHats / ξHats) - ξHats - 1)  # Calculate log-likelihood at each estimate
+  @tullio grad=false weights[y] := exp(logLikelihood[x] - logLikelihood[y]) |> inv # Calculate weights from log-likelihood
 
-    return ξ, σ
+  θHat = weights ⋅ θHats  # Take the dot product of weights and pointwise estimates of θ to get the full estimate
+
+  ξ = mean(log1p.(- θHat .* sample))
+  σ = -ξ / θHat
+  # Drag towards .5 to reduce variance for small n
+  if wip
+    ξ = (ξ * n + .5 * n_0) / (n + n_0) 
+  end
+
+  return ξ, σ
 
 end
 
@@ -79,9 +80,8 @@ end
 @return Vector of quantiles.
 """
 function gpd_quantile(p::Real, k::Real, sigma::Real)
-  return sigma * expm1(-k * log1p(-p)) / k
+  return @fastmath sigma * expm1(-k * log1p(-p)) / k
 end
-
 
 
 end
