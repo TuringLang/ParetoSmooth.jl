8bda020a376c58affe34ea807046fc21309bec3b
Closed-Limelike-Curves <paradac@carleton.edu>
2021-08-03 19:06:26 -0700

    stuff

--- src/MomentMatch.jl
+++ src/MomentMatch.jl
@@ -5,7 +5,7 @@ using Tables
 using Tullio
 
 """
-    adapt_moments(
+    adapt_cv(
         log_target::Function,
         psis_object::Psis,
         samples::AbstractArray,
@@ -19,11 +19,13 @@ Perform importance-weighted moment matching, adapting a sample from a proposal d
 to more closely match the target distribution.
 
 # Arguments
-  - `log_target`: The log-pdf of the target distribution, described as a function of the 
+  - `log_target`: The log-pdf of the target distribution. This should be a function having
+  θ, a vector of parameters, as its input; and `x`, the data set, as its second input.
+  
 """
-function adapt_moments(
+function adapt_cv(
     log_target::Function,
-    psis_object::Psis,
+    psis_object::AbstractCV,
     samples::AbstractArray,
     data;
     hard_thresh::Real = 2/3,
@@ -31,17 +33,28 @@ function adapt_moments(
     soft_cap::Integer = 10
 )
 
-    psis_object = Psis()
     dims = size(samples)
     n_steps, n_params, n_chains = dims
     mcmc_count = n_steps * n_chains
+    log_count = mcmc_count
+
     weights = psis_object.weights
     ξ = psis_object.pareto_k
     resample_count = size(weights, 1)
     
     Threads.@threads @inbounds for resample in 1:resample_count
-        @views log_proposal = weights[resample, :, :]
-        _match!(log_target, log_proposal, samples, ξ; hard_thresh, soft_thresh, soft_cap)
+        log_proposal = log_proposal
+        _match!(
+            log_target,
+            log_proposal,
+            view(weights, resample, :, :),
+            samples,
+            ξ[resample],
+            log_count,
+            hard_thresh,
+            soft_thresh,
+            soft_cap
+        )
     end
 
 end
@@ -50,14 +63,14 @@ end
 function _match!(
     log_target::Function,
     log_proposal::AbstractArray,
+    weights::AbstractArray,
     θ_hats::AbstractArray,
     ξ::Real,
-    hard_thresh::Real = 2/3,
-    soft_thresh::Real = 1/2,
-    soft_cap::Integer = 10,
+    log_count::Real,
+    hard_thresh::Real,
+    soft_thresh::Real,
+    soft_cap::Integer,
 )
-    dims = size(θ_hats)
-    mcmc_count = size(θ_hats, :parameter)
 
     # initialize variables
     num_iter = 0  # iterations of IWMM
@@ -68,13 +81,11 @@ function _match!(
     μ_proposed = similar(μ)
     σ = std(θ_hats; dims=:parameter)
     σ_proposed = similar(σ)
-    weights = 
-    weights_proposed = similar(log_proposal)
-    
+    weights_proposed = similar(weights)
+
 
     while _keep_going(ξ, hard_thresh, soft_thresh, soft_cap, num_iter)
-        
-        
+
         if transform == 1
             μ_proposed = mean(θ_hats, weights; dims=2)
             σ_proposed .= σ
@@ -89,22 +100,27 @@ function _match!(
         end
 
         θ_proposed = (θ_hats + μ_proposed - μ) * (σ_proposed * inv(σ)) 
-        @. weights_proposed = _safe_exp(log_target(θ_proposed) - log_proposal)
-        ξ_proposed = _psis_smooth!(weights_proposed)
+        @. weights_proposed = _safe_exp(log_target(θ_proposed) - log_proposal, log_count)
+        ξ_proposed = _psis_smooth!(weights)
 
         if ξ_proposed < ξ
             num_iter += 1
             _normalize!(weights_proposed)
 
             ξ = ξ_proposed
-            μ = μ_proposed
-            σ = σ_proposed
-            θ_hats = θ_proposed
+            @. begin
+                μ = μ_proposed
+                σ = σ_proposed
+                θ_hats = θ_proposed
+                weights = weights_proposed
+            end
         else
             transform += 1
         end
 
     end
+
+    return weights
 
 end
 
@@ -131,6 +147,6 @@ end
 Safely exponentiate x, preventing underflow/overflow by rescaling all elements 
 by a common factor
 """
-function _safe_exp(x)
-    return exp(x - $maximum(x; dims=2) + log(length(x)))
+function _safe_exp(x, log_count)
+    return @. exp(x - $maximum(x; dims=2) + log_count)
 end
