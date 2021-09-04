188c45e66f31f9e303ffd24416da8a06b4565937
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-05 06:24:29 +1000

    setup docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(ParetoSmooth, :DocTestSetup, :(using ParetoSmooth); recursiv
 makedocs(;
     modules=[ParetoSmooth],
     authors="Carlos Parada <paradac@carleton.edu>",
-    repo="https://github.com/ParadaCarleton/ParetoSmooth.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/TuringLang/ParetoSmooth.jl/blob/{commit}{path}#{line}",
     sitename="ParetoSmooth.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ParadaCarleton.github.io/ParetoSmooth.jl",
+        canonical="https://TuringLang.github.io/ParetoSmooth.jl",
         assets=String[],
     ),
     pages=[
@@ -18,6 +18,10 @@ makedocs(;
     ],
 )
 
-deploydocs(;
-    repo="github.com/ParadaCarleton/ParetoSmooth.jl",
-)
+if get(ENV, "CI", "false") == "true"
+    deploydocs(;
+        repo="github.com/TuringLang/ParetoSmooth.jl",
+        push_preview=true,
+    )
+end
+
